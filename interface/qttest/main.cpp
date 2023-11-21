3bb0b69688bf2a4bcb4c210e0abcd6135e7cc239
Vittorio_Toffolutti <97800904+ToffoluttiVittorio@users.noreply.github.com>
2023-11-21 10:24:13 +0100

    Add files via upload

---
+++ interface/qttest/main.cpp
@@ -0,0 +1,17 @@
+#include <QTest>
+
+class TestQString: public QObject
+{
+    Q_OBJECT
+private slots:
+    void toUpper();
+};
+
+void TestQString::toUpper()
+{
+    QString str = "Hello";
+    QVERIFY(str.toUpper() == "HELLO");
+}
+
+QTEST_MAIN(TestQString)
+#include "main.moc"
