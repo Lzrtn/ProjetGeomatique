b9786af4ed3a5afe992b42adca248b41bd9502a2
ToffoluttiVittorio <vittorio.toffolutti@gmail.com>
2023-11-21 10:21:14 +0100

    Ajout des test pour le front

---
+++ interface/googletest/google.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+TEST(myVectorTest, constructor_HappyPath) {
+
+    //assert
+    EXPECT_EQ (0,0);
+}
+
+TEST(myVectorTest, constructor_NegativeValue) {
+    //arrange
+
+    //assert
+    EXPECT_EQ (1, 1);
+}
+
