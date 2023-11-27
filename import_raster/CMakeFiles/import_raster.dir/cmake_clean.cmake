a9187d8e554242b9d16d1aa01f74f45360b8397b
AxelDmt <118598896+AxelDmt@users.noreply.github.com>
2023-11-27 09:28:53 +0100

    add import raster files (only op for wms-raster flow)

---
+++ import_raster/CMakeFiles/import_raster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/import_raster.dir/import_raster.cpp.o"
+  "CMakeFiles/import_raster.dir/import_raster.cpp.o.d"
+  "import_raster"
+  "import_raster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/import_raster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
