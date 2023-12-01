edb8b2ad9f85c2e148dc0370da2bdcf3e5e40dbf
ToffoluttiVittorio <vittorio.toffolutti@gmail.com>
2023-12-01 16:37:56 +0100

    Setup pour build

--- setup.sh
+++ setup.sh
@@ -7,19 +7,27 @@ function exit_with_error {
 }
 
 # Dossier du projet
-PROJET_DIR="ProjetGeomatique"
+current_directory="$PWD"
+
+sudo chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Installer les dépendances nécessaires
-sudo apt-get update || exit_with_error "Échec de la mise à jour des paquets"
+#sudo apt-get update || exit_with_error "Échec de la mise à jour des paquets"
 sudo apt-get install -y gdal-bin libgdal-dev libpq-dev libpqxx-dev libgtest-dev xvfb || exit_with_error "Échec de l'installation des dépendances"
 sudo apt-get install build-essential qt5-qmake qtbase5-dev qtchooser qtbase5-dev-tools
 sudo apt-get install postgresql-14-postgis-3
 sudo apt-get install postgresql-15-postgis-3
 sudo apt-get install postgresql-16-postgis-3
+
 sudo apt-get install curl
+
+
+
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo usermod -a -G docker $USER || exit_with_error "Échec de l'ajout de l'utilisateur au groupe docker"
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo apt-get install zenity
 
@@ -48,7 +56,6 @@ sudo docker stop database-tisig
 # cd "$PROJET_DIR" || exit_with_error "Échec du changement de répertoire vers $PROJET_DIR"
 
 # Ajouter l'utilisateur au groupe docker
-sudo usermod -a -G docker $USER || exit_with_error "Échec de l'ajout de l'utilisateur au groupe docker"
 
 #Build
 mkdir -p build 
@@ -80,8 +87,9 @@ sudo bash -c 'cat <<EOL > /usr/share/applications/TiSIG.desktop
 [Desktop Entry]
 Name=TiSIG
 Comment=MiniSIG
-Exec=/home/formation/Bureau/ProjetGeomatique-4/exec.sh
-Icon=/home/formation/Bureau/ProjetGeomatique-4/logotisig.png
+Path='$current_directory'/build/src
+Exec='$current_directory'/ProjetGeomatique/build/src/src
+Icon='$current_directory'/ProjetGeomatique/logotisig.png
 Terminal=False
 Type=Application
 Categories=Application;
