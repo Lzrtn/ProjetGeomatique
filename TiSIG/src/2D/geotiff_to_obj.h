12bc7972efb8f2731230c9d1e72d811f89f58a26
outmrhoust <hicham.outmrhoust@ensg.eu>
2023-12-05 12:25:24 +0100

    export multiple obj files from mnt

--- TiSIG/src/2D/geotiff_to_obj.h
+++ TiSIG/src/2D/geotiff_to_obj.h
@@ -14,17 +14,9 @@
 class GeoTiffToObjConverter {
 public:
     GeoTiffToObjConverter(const std::string inputFilePath,const std::string inputOrthoFilePath, const std::string outputObjFilePath);
-    GeoTiffToObjConverter(const std::string inputFilePath, const std::string outputObjFilePath);
     ~GeoTiffToObjConverter();
 
-    void writeObjFileWithTextures();
-    void writeCoordWithTextures();
-    void writeObjFileWithoutTextures();
-    // void writeObjFiles();
-
-    std::vector<QVector3D> getPosition() { return m_position; };
-    std::vector<QVector3D> getNormal(){return m_normal; };
-    std::vector<QVector2D> getTexture(){return m_textCoord; };
+    std::string writeObjFileWithTextures();
 
 
 private:
@@ -33,8 +25,4 @@ private:
     const std::string outputObjFilePath;
     GDALDataset* dataset;
     GDALDataset* orthoDataset;
-
-    std::vector<QVector3D> m_position;
-    std::vector<QVector3D> m_normal;
-    std::vector<QVector2D> m_textCoord;
 };
