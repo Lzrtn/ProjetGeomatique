3ed7d683f32b6f2b7d9be051e0691db6405e150e
VGiudicelli <vincent.giudicelli@ensg.eu>
2023-12-07 16:14:09 +0100

    add comments

--- TiSIG/src/3D/exempleobject3dstorage.h
+++ TiSIG/src/3D/exempleobject3dstorage.h
@@ -7,19 +7,42 @@
  * @brief The ExempleObject3DStorage class
  *
  * Exemple of Object3DStorage
+ *
+ * @see Object3DStorage
  */
 class ExempleObject3DStorage : public Object3DStorage
 {
 public:
+	/**
+	 * @brief ExempleObject3DStorage
+	 * Make a layer as example and tests, with buildings and mnt
+	 * @see Building3D
+	 * @see MNT3D
+	 * @see Object3DStorage
+	 */
 	ExempleObject3DStorage();
+	/**
+	 * @brief destructor
+	 * @see ExempleObject3DStorage::ExempleObject3DStorage (constructor)
+	 */
 	~ExempleObject3DStorage();
 
+	/**
+	 * @brief GetObjectsInEmprise
+	 *
+	 * @see Object3DStorage::GetObjectsInEmprise
+	 */
 	void GetObjectsInEmprise(
 			const Emprise &/*emprise*/,
 			std::map<int, Object3DFactory*> &new_buildings,
 			std::vector<int> &show_objects,
 			std::vector<int> &forget_objects);
 
+	/**
+	 * @brief PickingObjectInfo
+	 *
+	 * @see Object3DStorage::PickingObjectInfo
+	 */
 	bool PickingObjectInfo(
 			const QVector3D &p1,
 			const QVector3D &p2,
