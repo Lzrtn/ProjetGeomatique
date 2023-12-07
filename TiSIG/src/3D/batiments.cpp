c06df354bab76a65ec2cee1810c530bc7a72481a
VGiudicelli <vincent.giudicelli@ensg.eu>
2023-12-07 14:18:14 +0100

    comment cout

--- TiSIG/src/3D/batiments.cpp
+++ TiSIG/src/3D/batiments.cpp
@@ -5,135 +5,136 @@
 #include <sstream>
 
 Batiments::Batiments(std::string ip)
-    :ipAddress(ip)
+	:ipAddress(ip)
 {
+	this->setCameraInitPosition({1839312.84, 5173874.29, 278.99});
 }
 
 using json = nlohmann::json;
 
 void Batiments::GetObjectsInEmprise(
-        const Emprise &emprise,
-        std::map<int, Object3DFactory*> &new_objects,
-        std::vector<int> &show_objects,
-        std::vector<int> &forget_objects)
+		const Emprise &emprise,
+		std::map<int, Object3DFactory*> &new_objects,
+		std::vector<int> &show_objects,
+		std::vector<int> &forget_objects)
 {
 
-    std::vector<std::string> listIndex = getListParentID(emprise);
+	std::vector<std::string> listIndex = getListParentID(emprise);
 
-    //show_objects.clear();
-    show_objects = {};
-    forget_objects.clear();
-    new_objects.clear();
+	//show_objects.clear();
+	show_objects = {};
+	forget_objects.clear();
+	new_objects.clear();
 
-    std::cout << "Contenu de listIndex : ";
-       for (const auto& index : listIndex) {
-           std::cout << index << " ";
-       }
-       std::cout << std::endl;
+	//std::cout << "Contenu de listIndex : ";
+	for (const auto& index : listIndex) {
+		//std::cout << index << " ";
+	}
+	//std::cout << std::endl;
 
-    for (const auto& index : listIndex) {
-        int buildingID = stoi(index);
+	for (const auto& index : listIndex) {
+		int buildingID = stoi(index);
 
-        show_objects.push_back(buildingID);
+		show_objects.push_back(buildingID);
 
-        // Check if buildingID is already in indexStorage
-        if (std::find(indexStorage.begin(), indexStorage.end(), buildingID) == indexStorage.end()) {
-            indexStorage.push_back(buildingID);
+		// Check if buildingID is already in indexStorage
+		if (std::find(indexStorage.begin(), indexStorage.end(), buildingID) == indexStorage.end()) {
+			indexStorage.push_back(buildingID);
 
-            auto buildingIt = buildings.find(buildingID);
-            if (buildingIt != buildings.end()) {
-                new_objects.emplace(buildingID, buildingIt->second);
-            } else {
-                std::vector<QVector3D> position = bat.createBuilding(index, ipAddress);
-                Building3DFactory *building = new Building3DFactory(position, position, textCoord, textPath);
-                buildings.emplace(buildingID, building);
-                new_objects.emplace(buildingID, building);
-            }
-        }
-    }
+			auto buildingIt = buildings.find(buildingID);
+			if (buildingIt != buildings.end()) {
+				new_objects.emplace(buildingID, buildingIt->second);
+			} else {
+				std::vector<QVector3D> position = bat.createBuilding(index, ipAddress);
+				Building3DFactory *building = new Building3DFactory(position, position, textCoord, textPath);
+				buildings.emplace(buildingID, building);
+				new_objects.emplace(buildingID, building);
+			}
+		}
+	}
 
-    for (auto it = buildings.begin(); it != buildings.end();) {
-        int buildingID = it->first;
-        if (std::find(show_objects.begin(), show_objects.end(), buildingID) == show_objects.end()) {
-            forget_objects.push_back(buildingID);
+	for (auto it = buildings.begin(); it != buildings.end();) {
+		int buildingID = it->first;
+		if (std::find(show_objects.begin(), show_objects.end(), buildingID) == show_objects.end()) {
+			forget_objects.push_back(buildingID);
 
-            if (it->second) {
-                delete it->second;
-            }
+			if (it->second) {
+				delete it->second;
+			}
 
-            it = buildings.erase(it);
-        } else {
-            ++it;
-        }
-    }
+			it = buildings.erase(it);
+		} else {
+			++it;
+		}
+	}
 
-    sort( show_objects.begin(), show_objects.end() );
-    show_objects.erase( unique( show_objects.begin(), show_objects.end() ), show_objects.end() );
+	sort( show_objects.begin(), show_objects.end() );
+	show_objects.erase( unique( show_objects.begin(), show_objects.end() ), show_objects.end() );
 
-    std::cout << "Taille index : " << indexStorage.size() << std::endl;
-    std::cout << "Taille show  : " << show_objects.size() << std::endl;
-    std::cout << "Taille forget  : " << forget_objects.size() << std::endl;
-    std::cout << "Taille new  : " << new_objects.size() << std::endl;
+	//std::cout << "Taille index : " << indexStorage.size() << std::endl;
+	//std::cout << "Taille show  : " << show_objects.size() << std::endl;
+	//std::cout << "Taille forget  : " << forget_objects.size() << std::endl;
+	//std::cout << "Taille new  : " << new_objects.size() << std::endl;
 }
 
 
 
 Batiments::~Batiments()
 {
-    for (auto pair: this->buildings) {
-        delete pair.second;
-    }
+	for (auto pair: this->buildings) {
+		delete pair.second;
+	}
 
 }
 
 
 std::vector<std::string> Batiments::getListParentID(const Emprise &emprise)
 {
-    std::vector<std::string> listIDtoDraw;
+	std::vector<std::string> listIDtoDraw;
 
-    try {
-        DbManager manager("CityGML", ipAddress);
-        pqxx::connection conn(manager.getString());
+	try {
+		DbManager manager("CityGML", ipAddress);
+		pqxx::connection conn(manager.getString());
 
-        if (conn.is_open()) {
+		if (conn.is_open()) {
 
-            std::cout << "Requête liste parent_id" << std::endl;
+			//std::cout << "Requête liste parent_id" << std::endl;
 
-            std::ostringstream ss;
-            ss << "SELECT parent_id FROM surface_geometry "
-               << "JOIN cityobject ON cityobject.id = surface_geometry.cityobject_id "
-               << "WHERE ST_Intersects(geometry, ST_Polygon(ST_GeomFromText('LINESTRING(";
+			std::ostringstream ss;
+			ss << "SELECT parent_id FROM surface_geometry "
+			   << "JOIN cityobject ON cityobject.id = surface_geometry.cityobject_id "
+			   << "WHERE ST_Intersects(geometry, ST_Polygon(ST_GeomFromText('LINESTRING(";
 
-            for (const auto& point : {emprise.g_a, emprise.g_b, emprise.g_c, emprise.g_d}) {
+			for (const auto& point : {emprise.g_a, emprise.g_b, emprise.g_c, emprise.g_d}) {
 
-                ss << std::fixed << std::setprecision(6) << point.x() << " " << point.y() << ", ";
-            }
+				ss << std::fixed << std::setprecision(6) << point.x() << " " << point.y() << ", ";
+			}
 
-            ss << std::fixed << std::setprecision(6) << emprise.g_a.x() << " " << emprise.g_a.y();
+			ss << std::fixed << std::setprecision(6) << emprise.g_a.x() << " " << emprise.g_a.y();
 
-            ss << ")'), 4171)) AND cityobject.objectclass_id = '25' GROUP BY parent_id;";
+			ss << ")'), 4171)) AND cityobject.objectclass_id = '25' GROUP BY parent_id;";
 
-            std::string query = ss.str();
+			std::string query = ss.str();
 
-            std::cout << query;
+			//std::cout << query;
 
-            manager.Request(query);
+			manager.Request(query);
 
 
-            for (const auto& row : manager.getResult()) {
-                std::string id = row[0].as<std::string>();
-                listIDtoDraw.push_back(row[0].as<std::string>());
-            }
+			for (const auto& row : manager.getResult()) {
+				std::string id = row[0].as<std::string>();
+				listIDtoDraw.push_back(row[0].as<std::string>());
+			}
 
-            conn.disconnect();
-        } else {
-            std::cerr << "Impossible de se connecter à la base de données." << std::endl;
-        }
-    } catch (const std::exception &e) {
-        std::cerr << "Erreur : " << e.what() << std::endl;
-    }
+			conn.disconnect();
+		} else {
+			std::cerr << "Impossible de se connecter à la base de données." << std::endl;
+		}
+	} catch (const std::exception &e) {
+		std::cerr << "Erreur : " << e.what() << std::endl;
+	}
 
-    return listIDtoDraw;
+	return listIDtoDraw;
 
 }
 
